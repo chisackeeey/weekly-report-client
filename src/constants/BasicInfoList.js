f0bb6e1c5994d2f4bef4f10af7c64198896d11f7
hirai chisaki <hirai@itpadminnoMacBook-Pro.local>
2020-01-07 16:48:38 +0900

    基本情報一覧画面作成

---
+++ src/constants/BasicInfoList.js
@@ -0,0 +1,25 @@
+const basicInfoList = [
+  {
+    projectId: 1,
+    name: "総合評定",
+    deadline: "2020年3月",
+    leader: "黒川SD",
+    member: "竹林"
+  },
+  {
+    projectId: 2,
+    name: "キャリアデザインシート",
+    deadline: "2020年3月",
+    leader: "黒川SD",
+    member: "竹林"
+  },
+  {
+    projectId: 3,
+    name: "キャリアデザインシート",
+    deadline: "2020年3月",
+    leader: "黒川SD",
+    member: "竹林"
+  }
+];
+
+export default basicInfoList;
---
+++ src/pages/documents/basicInfo/index.js
@@ -0,0 +1,35 @@
+import { Button, Table } from "react-bootstrap";
+import Router from "next/router";
+import Layout from "src/components/Layout";
+import basicInfoList from "src/constants/BasicInfoList";
+
+function BasicInfo() {
+  return (
+    <Layout>
+      <h2>基本情報編集</h2>
+      <Table striped bordered condensed hover>
+        <thead>
+          <tr>
+            <th>案件</th>
+            <th>リリース期限</th>
+            <th>担当役席</th>
+            <th>メンバー</th>
+            <th>進捗状況</th>
+          </tr>
+        </thead>
+        {basicInfoList.map(({ projectId, name, deadline, leader, member }) => (
+          <tbody key={projectId}>
+            <tr>
+              <td>{name}</td>
+              <td>{deadline}</td>
+              <td>{leader}</td>
+              <td>{member}</td>
+            </tr>
+          </tbody>
+        ))}
+      </Table>
+    </Layout>
+  );
+}
+
+export default BasicInfo;
