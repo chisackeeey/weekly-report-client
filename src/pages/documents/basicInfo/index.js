07014e723301f23c8b6d9cbaf0518c5c3faad52c
hirai chisaki <hirai@itpadminnoMacBook-Pro.local>
2020-01-08 13:53:11 +0900

    ステータスボタンを追加

--- src/pages/documents/basicInfo/index.js
+++ src/pages/documents/basicInfo/index.js
@@ -1,8 +1,10 @@
-import { Button, Table } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, FormControl, Table } from "react-bootstrap";
 import Router from "next/router";
+import _ from "lodash";
 import BackToTopButton from "src/components/BackToTopButton";
 import Layout from "src/components/Layout";
-import basicInfoList from "src/constants/BasicInfoList";
+import importBasicInfoList from "src/constants/BasicInfoList";
 
 const edit = e => {
   const id = e.target.value;
@@ -10,6 +12,28 @@ const edit = e => {
 };
 
 function BasicInfo() {
+  const [basicInfoList, setBasicInfoList] = useState(importBasicInfoList);
+
+  function close(e) {
+    const list = _.cloneDeep(basicInfoList);
+    list.map(({ basicInfo }) => {
+      if (basicInfo.projectId == e.target.id) {
+        basicInfo.status = "close";
+      }
+    });
+    setBasicInfoList(list);
+  }
+
+  function open(e) {
+    const list = _.cloneDeep(basicInfoList);
+    list.map(({ basicInfo }) => {
+      if (basicInfo.projectId == e.target.id) {
+        basicInfo.status = "open";
+      }
+    });
+    setBasicInfoList(list);
+  }
+
   return (
     <Layout>
       <h2>基本情報編集</h2>
@@ -44,14 +68,65 @@ function BasicInfo() {
               </td>
               <td>
                 {basicInfo.status === "open" && (
-                  <Button bsStyle='danger' onClick={}>
+                  <Button
+                    bsStyle='danger'
+                    id={basicInfo.projectId}
+                    onClick={close}
+                  >
                     close
+                  </Button>
+                )}
+                {basicInfo.status === "close" && (
+                  <Button
+                    bsStyle='primary'
+                    id={basicInfo.projectId}
+                    onClick={open}
+                  >
+                    open
                   </Button>
                 )}
               </td>
             </tr>
           </tbody>
         ))}
+      </Table>
+      <Table striped bordered condensed hover>
+        <thead>
+          <tr>
+            <th>案件</th>
+            <th>リリース期限</th>
+            <th>担当役席</th>
+            <th>メンバー</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <FormControl componentClass='textarea' id='name' name='name' />
+            </td>
+            <td>
+              <FormControl
+                componentClass='textarea'
+                id='deadline'
+                name='deadline'
+              />
+            </td>
+            <td>
+              <FormControl
+                componentClass='textarea'
+                id='leader'
+                name='leader'
+              />
+            </td>
+            <td>
+              <FormControl
+                componentClass='textarea'
+                id='member'
+                name='member'
+              />
+            </td>
+          </tr>
+        </tbody>
       </Table>
       <BackToTopButton />
     </Layout>
