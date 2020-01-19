2d8f20388e8d4700a2304e83690303adc7234587
chisackeeey <chisaki_h@outlook.jp>
2020-01-19 19:48:34 +0900

    ヘッダーの修正

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,16 +10,12 @@ function Header() {
     <Navbar fluid>
       <Navbar.Header>
         <Navbar.Brand>
-          <a href='/documents'>週次進捗会</a>
+          <a href="/documents">週次進捗会</a>
         </Navbar.Brand>
       </Navbar.Header>
       <Navbar.Collapse>
         <Nav pullRight>
-          <NavDropdown
-            href='/documents/basicInfo'
-            title='メニュー'
-            id='basic-nav-dropdown'
-          >
+          <NavDropdown title="メニュー" id="basic-nav-dropdown">
             <MenuItem onClick={basicInfo}>基本情報一覧</MenuItem>
           </NavDropdown>
         </Nav>
